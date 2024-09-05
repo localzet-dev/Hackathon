885a9642df5761d66eb5fc3c17d7dbeec835088d
Ivan Zorin <creator@localzet.com>
2024-09-05 14:54:00 +0300

    Update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,21 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { imagetools } from 'vite-imagetools'
+import {imagetools} from 'vite-imagetools'
+import {viteObfuscateFile} from 'vite-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     define: {
         'process.env': process.env
     },
-    plugins: [vue(), imagetools()],
+    plugins: [
+        vue(), imagetools(),
+        viteObfuscateFile({
+            globalOptions: {
+                compact: true,
+            },
+        }),
+    ],
     resolve: {
         alias: [
             {
